e9bf289386c9fd92e1ccc67f4037f4daf40e4b07
adam <syahnetwork@gmail.com>
2019-07-08 16:52:37 +0700

    fix5

--- index.php
+++ index.php
@@ -139,7 +139,7 @@ if (isset($_POST['submit'])) {
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
         integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM"
         crossorigin="anonymous"></script>
-<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.0/jquery.min.js"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.9.0/jquery.min.js"></script>
 <script>
     $("#result").hide();
 </script>
